7cd584641f15febe3246f00b9112626b100831d4
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-02 19:02:44 +0530

    generate the encryptmsg

--- backend/EmitterService/EncryptedData.js
+++ backend/EmitterService/EncryptedData.js
@@ -1,10 +1,16 @@
 const { log } = require("console");
 const helper = require("./util/helper.js");
 const crypto = require("crypto");
+
+const bcrypt = require("bcrypt");
 const fs = require("fs");
+const { type } = require("os");
 const getRandomInt = helper.getRange;
 const getRandomElementsFromArray = helper.getRadomElement;
-
+const advanceEncrypt = helper.advanceEncryptFun;
+// console.log(advanceEncrypt);
+// return;
+require("dotenv").config();
 /**
  * !This File Contain the Login for Encrypt the message
  *
@@ -16,7 +22,6 @@ const getRandomElementsFromArray = helper.getRadomElement;
  * * Read The data Use Random Number to determine the number of msg(49-499)
  *
  */
-
 function generateRandomMessage() {
   /**
    * * get a random interger Range
@@ -24,20 +29,61 @@ function generateRandomMessage() {
    * !getNumberInt()
    */
   const reableStream = fs.readFileSync("data.json");
-  console.log(reableStream);
+  //   console.log(reableStream);
   // as we get a buffer data or stram data parse of String
   const data = JSON.parse(reableStream);
-  console.log(data);
   //   console.log(data);
-  const msgRange = getRandomInt(49, 499);
+  //   console.log(data);
+  const msgRange = getRandomInt(3, 6);
 
   /**
    * * As we have get the range as per the range we need to define the number of message
    * !{name,origin,destination}
+   * !Define data streamData and concatination with |
    */
+  let streamingData = "";
   for (let i = 0; i < msgRange; i++) {
     const eleData = getRandomElementsFromArray(data.names, data.cities);
-    console.log(eleData);
+    // console.log(eleData);
+    /**
+     * !Covert the object to json(jsavScript Notation)
+     */
+    const dataNotation = JSON.stringify(eleData);
+    // console.log(dataNotation);
+    /**
+     * !generate the hash value form the json data for per single session
+     */
+    const hashValue = crypto
+      .createHash("sha256")
+      .update(dataNotation)
+      .digest("hex");
+    // console.log(hashValue.length)
+
+    /**
+     * ! we have get the hashValue which is used as scret key and store in eleData
+     */
+    const secretKey = hashValue;
+    eleData.secretKey = hashValue;
+    // console.log(eleData);
+    /**
+     *  TODO HASH the pass key
+     */
+    let passkey = process.env.PASS__KEY;
+
+    let saltValue = process.env.SALT;
+    const key = crypto.pbkdf2Sync(passkey, saltValue, 100000, 32, "sha256");
+    // console.log(key.toString("hex"));
+    const encryptHasedValue = advanceEncrypt(eleData, key);
+    // console.log(encryptHasedValue);
+
+    // return hashValue;
+    streamingData += encryptHasedValue;
+    streamingData += "|";
   }
+  console.log(typeof streamingData);
+  streamingData = streamingData.slice(0, -1);
+  return streamingData;
 }
-generateRandomMessage();
+// console.log(hashValue);
+const hashValue = generateRandomMessage();
+console.log(hashValue);
--- backend/EmitterService/util/helper.js
+++ backend/EmitterService/util/helper.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+const fs = require("fs");
 function getRandomInt(max, min) {
   let RandomNumber = Math.floor(Math.random() * (max - min) + min);
   return RandomNumber;
@@ -15,5 +17,27 @@ function getRandomElementsFromArray(namesArray, citiesArray) {
     destination: destination,
   };
 }
+
+function advanceEncrypt(data, key) {
+  /**
+   * Need IV initial Vector
+   * ciper
+   */
+  //   console.log(typeof data);
+
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
+  /**
+   * TODO String
+   */
+  let stringData = typeof data === "string" ? data : JSON.stringify(data);
+  //   console.log(stringData);
+  let encData = cipher.update(stringData, "utf-8", "base64");
+  encData += cipher.final("base64");
+  //   console.log(encData);
+  //   return Buffer.concat([iv, Buffer.from(encData, "base64")]).toString("base64");
+  return encData;
+}
 module.exports.getRange = getRandomInt;
 module.exports.getRadomElement = getRandomElementsFromArray;
+module.exports.advanceEncryptFun = advanceEncrypt;
